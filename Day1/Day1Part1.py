08a4553056907b8e511858dd4f2952efcf3c85ef
James <jamesehess@gmail.com>
2023-12-04 15:48:51 -0700

    Completed Day 1

--- Day1/Day1Part1.py
+++ Day1/Day1Part1.py
@@ -18,20 +18,10 @@ with open("Day1\Day1Input.txt") as file:
 for input in inputs:
 
 
-    firstnumber = re.search('[0-9]|one|two|three|four|five|six|seven|eight|nine', input).group()
-    lastnumber = re.findall('[0-9]|one|two|three|four|five|six|seven|eight|nine', input)
+    firstnumber = re.search('[0-9]', input).group()
+    lastnumber = re.findall('[0-9]', input)
 
     number = firstnumber + lastnumber[-1]
-    number = number.replace("one", "1")
-    number = number.replace("two", "2")
-    number = number.replace("three", "3")
-    number = number.replace("four", "4")
-    number = number.replace("five", "5")
-    number = number.replace("six", "6")
-    number = number.replace("seven", "7")
-    number = number.replace("eight", "8")
-    number = number.replace("nine", "9")
-
     numbers.append(int(number))
 
 
